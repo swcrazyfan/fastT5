13035223d871d63e4a5fb063affbabd819d91766
swcrazyfan <79735340+swcrazyfan@users.noreply.github.com>
2022-02-27 13:36:37 +0800

    fix ONNX

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setuptools.setup(
     install_requires=[
         "torch>=1.7.0,!=1.8.0",  # excludes torch v1.8.0
         "onnx",
-        "onnxruntime==1.7.0",
+        "onnxruntime>=1.7.0",
         "transformers==4.15.0",
         "progress>=1.5",
         "sentencepiece",
